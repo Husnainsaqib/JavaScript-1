072e245a509d3576a8fe4553484594b73c31f30d
Husnainsaqib <husnain.mandhai2007@gmail.com>
2023-10-13 16:49:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+//         C H P# 1    (ALERTS) 
+
+
+// Q1. Write a script to greet your website visitor using JS alertbox."
+alert("HELLO USER")
+
+// Q2. Write a script to display following message on your webpage:
+alert("ERROR! Please enter a valid password")
+
+// Q3. Write a script to display following message on your webpage: (Hint : Use line break)"
+alert("Welcome to the JS land..\nHappy coding!")
+
+// Q5. Generate the following message through browser’s developer console:
+console.log(alert("I can run JS through my web Browsers console"));
+
+// Q6. Make use of alerts in your new/existing HTML & CSS project.
+// ALREADY USING.
+
+
+
+//            C H P# 2 (VARIABLES FOR STRINGS) 
+
+
+// Q1. Declare a variable called username.
+var userName = ""
+
+// Q2. Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = "Husnain Saqib"
+
+// Q3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+var message = "Hello World";
+alert(message);
+
+// Q4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+var bio = "Jhon Doe"
+var bio2 = "15 years old"
+var bio3 = "Certified Mobile App Development"
+alert(bio)
+alert(bio2)
+alert(bio3)
+
+// Q5. Write a script to display the following alert using one JS variable:
+var food = "Pizza\nPizz\nPiz\nPi\nP"
+alert(food)
+
+// Q6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).Show the blow mentioned message in an alert box.(Hint: usestring concatenation)
+var email = "husnain.mandhai2007@gmail.com"
+alert("my email address is"+" "+ email)
+
+// Q7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the Book"+ " " + book )
+
+// Q8. Write a script to display this in browser through JS
+// havent done yet
+
+// Q9. Store following string in a variable and show in alert and browser through JS
+var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(symbol)
+
+
+
+//         C H P#  3 (VARIABLES FOR NUMBERS)
+// Q1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+var age = 16
+alert("I am" + "" + age + " years old")
+
+// Q2. Declare & initialize a variable to keep track of how manytimes a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+// havent did it yet in class
+
+// Q3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+// Havent learned yet how to display on browser 
+
+//  Q4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.// 
+//CANT SOLVE IT
+
+
+
+//           C H P#  4 (VARIABLE NAMES: LEGAL & ILLEGAL)
+
+
+//Q1. Declare 3 variables in one statement.
+var bs = by = bl = 0
+
+//Q2. Declare 5 legal & 5 illegal variable names.
+//legal
+var geeks = ''
+var geeks_For_Geeks = ''
+var $Geeks = ''
+var _Geeks= ''
+var Geeks123 = ''
+//ILLEGAL
+// var Geeks-for-Geeks = ''
+// var 123Geeks = ''
+// var Geeks for Geeks = ''
+// var 421flat = ''
+// var nerd-0-0 = ''
